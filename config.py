ca5eedb8cf8316e563f68fb2bf87334c40803011
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2026-01-19 09:08:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("8260828039:AAGZDWSQyHKiTCFxP7Ub64c5IwrCU86IrFk")
+    BOT_TOKEN = os.environ.get("8579065326:AAH6m4ievy30DEe0VPbBIRoAxfABEstbUy4")
     API_ID = 22484497
     API_HASH = "c38cb053916c47a97590c244663cbaef"
     ADMIN_ID = [6252997817]
     DB_URL = "mongodb+srv://uploaderbot:uploaderbot@cluster0.mpesxpw.mongodb.net/?retryWrites=true&w=majority"
     DB_NAME = os.environ.get("uploaderbot")
-    TXT_LOG = -1003120656587
+    TXT_LOG = -1003171881798
     HOST = "https://www.masterapi.tech"
+
 
 
